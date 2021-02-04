0c386610db70b2a0365bc325b177bc2a613ad2f1
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2021-02-04 11:01:00 +0100

    allt klart!

--- HUPP2/upg2.m
+++ HUPP2/upg2.m
@@ -1,5 +1,4 @@
-clc, clf
-d = 10e-6;
+d = 20e-6;
 n_o = 1.5;
 N_skiv = 100;
 lambda_r = 650e-9;
@@ -8,10 +7,42 @@ lambda_b = 450e-9;
 thetas = linspace(0, pi/2, 100);
 E_0 = [1;0];
 
-hold on;
-plot(thetas, g(thetas, N_skiv, d, n_o, lambda_r, E_0), 'r')
-plot(thetas, g(thetas, N_skiv, d, n_o, lambda_g, E_0), 'g')
-plot(thetas, g(thetas, N_skiv, d, n_o, lambda_b, E_0), 'b')
+%% B)
+clf, hold on;
+r_vec = g(thetas, N_skiv, d, n_o, lambda_r, E_0);
+g_vec = g(thetas, N_skiv, d, n_o, lambda_g, E_0);
+b_vec = g(thetas, N_skiv, d, n_o, lambda_b, E_0);
+plot(thetas, r_vec, 'r')
+plot(thetas, g_vec, 'g')
+plot(thetas, b_vec, 'b')
+grid on
+xlabel('0° < \theta < 90°')
+ylabel('Intensitet I_{ut}')
+set(gca, 'XLim', [0,pi/2], 'XTick', 0:pi/18:pi/2, 'XTickLabel', 0:10:90);
+legend("R", "G", "B")
+
+%% D)
+clc, clf, hold on;
+ds = linspace(20e-6,1e-7, 200);
+
+rm = zeros(1);
+gm = zeros(1);
+bm = zeros(1);
+
+for i = 1:length(ds)
+    rm(i) = max(g(thetas, N_skiv, ds(i), n_o, lambda_r, E_0));
+    gm(i) = max(g(thetas, N_skiv, ds(i), n_o, lambda_g, E_0));
+    bm(i) = max(g(thetas, N_skiv, ds(i), n_o, lambda_b, E_0));
+end
+
+plot(ds, rm, 'r')
+plot(ds, gm, 'g')
+plot(ds, bm, 'b')
+grid on
+xlabel('d [m]')
+ylabel('Maximal Intensitet I_{ut}')
+legend("R", "G", "B")
+
 
 function y = g(thetas, N_skiv, d, n_o, lambda, E_0)
     for i = 1:100
